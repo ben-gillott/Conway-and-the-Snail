6d0f28731c3860c8387568cdc992eeac4afc46a6
Benjamin Gillott <bg357@cornell.edu>
2019-12-10 17:25:06 -0500

    progress (renders, basic CGL)

--- cgl.js
+++ cgl.js
@@ -4,19 +4,31 @@ function draw() {}
 
 var dim = 8;
 var pixwidth = 30;
+var stepLength = 4;
+var borderValue = 0; //Value used when sampling the borderland
+var flag = false;
 var arr = new Array();
+var arr2 = new Array();
 
 function buttonClick(){
-    setupArr();
-    drawCanvas();
+    if(!flag){
+        setupArr();
+        drawCanvas();
+        flag = true;
+    }else{
+        timestep();
+        drawCanvas();
+    }
 }
 
 function setupArr(){
     //Setup the array to be empty
     for(var x = 0; x < dim; x++){
         arr[x] = [];
+        arr2[x] = [];
         for(var y = 0; y < dim; y++){
-            arr[x][y] = Math.random()*250;
+            arr[x][y] = Math.floor(Math.random()*2); //Generate two colors (0,1)
+            arr2[x][y] = 0;
         }
     }
 }
@@ -29,10 +41,43 @@ function drawCanvas(){
 
     for(x = 0; x < dim; x++) {
         for(y = 0; y < dim; y++) {
-            var num = arr[x][y];
+            var rgb = colorFromValue(arr[x][y]);
 
-            context.fillStyle = "rgb(" + num + "," + num + "," + num + ")";
+            context.fillStyle = "rgb(" + rgb[0] + "," + rgb[1] + "," + rgb[2] + ")";
             context.fillRect(x*pixwidth, y*pixwidth, pixwidth, pixwidth);
         }
     }
+}
+
+function colorFromValue(v){
+    if (v == 0){
+        return [0,0,0];
+    }else if (v == 1){
+        return [250, 250, 250];
+    }
+    return [250, 0, 0];
+
+}
+
+function timestep(){
+    for(var x = 1; x < dim-1; x++){
+        for(var y = 1; y < dim -1; y++){
+            arr[x][y] = calculate(x,y);
+        }
+    }
+    // arr = arr2;
+}
+
+function calculate(x, y){
+    var num = arr[x+1][y+1]+arr[x+1][y]+arr[x+1][y-1]+arr[x][y+1]+arr[x][y-1]+arr[x-1][y+1]+arr[x-1][y]+arr[x-1][y-1];
+    //Any live cell with two or three neighbors survives.
+    //All other live cells die in the next generation. 
+    if(arr[x][y] == 1 && !(num == 2 || num == 3)){
+        return 0;
+    }
+    //Any dead cell with three live neighbors becomes a live cell.
+    else if(arr[x][y] == 0 && (num == 3)){
+        return 1;
+    }
+    return arr[x][y];
 }
